6341d57d180e2b50b5cdf13a2910669efd7c8009
unknown <t@trevornei.com>
2023-11-24 08:22:12 -0700

    Added color

--- Results-Summary-Component/src/App.jsx
+++ Results-Summary-Component/src/App.jsx
@@ -4,7 +4,7 @@ import MainScoreCont from './Components/MainScoreCont'
 export default function App() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center w-screen h-screen m-32 font-hg">
+      <div className="flex flex-col items-center justify-center w-screen h-screen font-hg bg-slate-200 m-0">
           <MainScoreCont />
       </div>
     </>
--- Results-Summary-Component/src/Components/Summary/SummarySubcategoryCont.jsx
+++ Results-Summary-Component/src/Components/Summary/SummarySubcategoryCont.jsx
@@ -15,7 +15,7 @@ export default function SummarySubcategoryCont() {
             <SubcategoryComponent image={MemoryIcon} />
             <SubcategoryComponent image={VerbalIcon} />
             <SubcategoryComponent image={VisualIcon} />
-            <button className='w-3/4 mx-auto mt-6 h-auto py-2 bg-gradient-to-t from-lightperp to-perp rounded-2xl text-white text-xl font-bold font-hg'>Continue</button> 
+            <button className='w-3/4 mx-auto mt-6 h-auto py-2 bg-gradient-to-t from-indigo-600 to-indigo-800 rounded-2xl text-white text-xl font-bold font-hg'>Continue</button> 
          </div>
     </>
   )
