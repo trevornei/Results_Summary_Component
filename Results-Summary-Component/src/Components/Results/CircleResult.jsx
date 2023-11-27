db1fa0cce592f51ed217b3fb9942ff205c660f37
unknown <t@trevornei.com>
2023-11-27 09:15:08 -0700

    Changed text size for circle result component that is rendered in the results container component.

--- Results-Summary-Component/src/Components/Results/CircleResult.jsx
+++ Results-Summary-Component/src/Components/Results/CircleResult.jsx
@@ -7,8 +7,8 @@ export default function CircleResult() {
             {/*TODO - Outputs:
                         1. Your Score: Variable Outcome
                         2. Out of 100: Constant */}
-            <h1 className='text-3xl font-bold text-white'>76</h1>
-            <h1 className='text-lg text-white'>Out of 100</h1>
+            <h1 className='text-3xl font-extrabold text-white'>76</h1>
+            <h3 className='text-sm text-white'>Out of 100</h3>
         </div>
     </>
   )
