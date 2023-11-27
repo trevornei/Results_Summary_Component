ad0df29b716b33da396804c61bfbe212613f761f
unknown <t@trevornei.com>
2023-11-27 09:19:45 -0700

    Added opacity to text in the results container child components.

--- Results-Summary-Component/src/Components/Results/ResultsCont.jsx
+++ Results-Summary-Component/src/Components/Results/ResultsCont.jsx
@@ -5,10 +5,10 @@ export default function ResultsCont() {
   return (
     <>
         <div className="flex flex-col items-center justify-center w-full h-full p-12 bg-gradient-to-b from-violet-700 to-indigo-800 md:rounded-3xl sm:rounded-b-3xl my-18 mx-18 ">
-            <h1 className="mx-auto mb-6 text-xl font-bold text-white font-hg">Your Result</h1>
+            <h1 className="mx-auto mb-6 text-xl font-bold text-white/60 font-hg">Your Result</h1>
             <CircleResult />
             <h1 className='mx-auto mt-6 text-3xl font-bold text-white'>Great!</h1>
-            <p className="mx-auto mt-6 text-center text-white text-md">Your score improved by 10% since your last attempt</p>
+            <p className="mx-auto mt-6 text-center text-white/60 text-md">Your results exceeded 65% of the people conducting the experiment!</p>
         </div>
     </>
   )
