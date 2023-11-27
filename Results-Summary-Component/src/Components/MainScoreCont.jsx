03011e44ec8635f363f53b59c4d582f3aad6c7ff
unknown <t@trevornei.com>
2023-11-27 10:04:58 -0700

    Added the animation-bounce to the score result.

--- Results-Summary-Component/src/Components/MainScoreCont.jsx
+++ Results-Summary-Component/src/Components/MainScoreCont.jsx
@@ -5,7 +5,7 @@ import SummaryCategoryCont from './Summary/SummaryCategoryCont'
 export default function SummaryCont() {
   return (
     <>
-        <div className="flex md:flex-row sm:flex sm:flex-col items-center justify-center rounded-3xl hover:shadow-lg hover:shadow-lightperp ease-in-out duration-300 delay-100 bg-white w-auto h-auto ">
+        <div className="flex items-center justify-center w-auto h-auto duration-300 ease-in-out delay-100 bg-white md:flex-row sm:flex sm:flex-col rounded-3xl hover:shadow-lg hover:shadow-violet-600/30 ">
             <ResultsCont />
             <SummaryCategoryCont />
         </div>
--- Results-Summary-Component/src/Components/Summary/SubcategoryComponent.jsx
+++ Results-Summary-Component/src/Components/Summary/SubcategoryComponent.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function SubcategoryComponent({ image, backgroundColor }) {
+export default function SubcategoryComponent({ image, backgroundColor, textColor }) {
   return (
-    <div className={`flex flex-row items-center justify-center w-full h-12 my-2 ${backgroundColor} rounded-xl`} >
+    <div className={`flex flex-row items-center justify-center w-full h-12 my-2 ${backgroundColor} rounded-xl `} >
         <img src={image} alt="Icon" />
-        <p className='px-3'>Subcategory</p>
-        <p className='px-3'>Score / 100</p>
+        <p className={`px-3 ${textColor}`}>Subcategory</p>
+        <p className='px-3 hover:animate-bounce hover:ease-in-out hover:delay-1000'>Score / 100</p>
     </div>
   )
 }
