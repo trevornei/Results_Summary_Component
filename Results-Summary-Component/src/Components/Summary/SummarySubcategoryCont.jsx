6da4e7642c51225941f764787f314d656c6eab38
unknown <t@trevornei.com>
2023-11-27 10:19:45 -0700

    Passed down the score of the results as props.

--- Results-Summary-Component/src/Components/Summary/SummarySubcategoryCont.jsx
+++ Results-Summary-Component/src/Components/Summary/SummarySubcategoryCont.jsx
@@ -11,10 +11,10 @@ export default function SummarySubcategoryCont() {
   return (
     <> 
         <div className="flex flex-col my-6">
-            <SubcategoryComponent image={ReactionIcon} backgroundColor={"bg-red-300/20"} textColor={"text-red-500"} />
-            <SubcategoryComponent image={MemoryIcon} backgroundColor={"bg-yellow-300/20"} textColor={"text-yellow-500"} />
-            <SubcategoryComponent image={VerbalIcon} backgroundColor={"bg-green-300/20"} textColor={"text-green-500"} />
-            <SubcategoryComponent image={VisualIcon} backgroundColor={"bg-blue-300/20"} textColor={"text-blue-500"} />
+            <SubcategoryComponent image={ReactionIcon} backgroundColor={"bg-red-300/20"} textColor={"text-red-500"} resultValue={"80"} />
+            <SubcategoryComponent image={MemoryIcon} backgroundColor={"bg-yellow-300/20"} textColor={"text-yellow-500"} resultValue={"92"} />
+            <SubcategoryComponent image={VerbalIcon} backgroundColor={"bg-green-300/20"} textColor={"text-green-500"} resultValue={"61"} />
+            <SubcategoryComponent image={VisualIcon} backgroundColor={"bg-blue-300/20"} textColor={"text-blue-500"} resultValue={"73"} />
             <button className='w-full h-auto py-2 mx-auto mt-6 font-bold text-white text-md bg-gradient-to-t from-violet-700 to-indigo-800 rounded-2xl font-hg'>Continue</button> 
          </div>
     </>
